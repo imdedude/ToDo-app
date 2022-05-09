5fbe5e83b209806f672e170a639831080dd26c37
Arek <arkadiusz.madejczyk@gmail.com>
2022-05-09 21:38:33 +0200

    first commit

---
+++ src/components/Task.vue
@@ -0,0 +1,19 @@
+<template>
+    <li>
+      <div>{{task.name}} {{index}}</div>
+      <small>{{task.dueDate}} @ {{task.dueTime}}</small>
+      <button @click="deleteTask(index)">X</button>
+    </li>
+
+</template>
+
+<script>
+export default {
+  props: ['task', 'index', 'deleteTask']
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/TodoPage.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-page padding>
+  <ul>
+    <task
+      v-for="(task,index) in tasks"
+          :task="task"
+          :index="index"
+          :deleteTask="deleteTask"
+          :key="id"
+    >
+    </task>
+  </ul>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+    data() {
+      return {
+        tasks: [
+          {
+            id: 1,
+            name: "Get bananas",
+            dueDate: '2019-05-02',
+            dueTime: '18:30'
+          },
+          {
+            id: 2,
+            name: "Get apples",
+            dueDate: '2019-05-02',
+            dueTime: '18:30'
+          },
+          {
+            id: 3,
+            name: "Go to shop",
+            dueDate: '2019-05-02',
+            dueTime: '18:30'
+          },
+
+        ]
+      }
+    },
+  methods: {
+      deleteTask(index){
+        this.tasks.splice(index,1)
+      }
+  },
+  components: {
+      'task' : require('components/Task.vue').default
+  }
+}
+</script>
+
+<style>
+
+</style>
